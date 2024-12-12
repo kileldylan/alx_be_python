938e68e76f82ac6452eb67d90ce94d9c93225bc4
Deelan Kilel <kileldylan@gmail.com>
2024-12-12 14:19:10 +0300

    added the rectangle_area.py file

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,10 @@
+#Area of a rectangle calculation
+
+#rectable variables
+length = 10
+width = 5
+
+#calculating the area
+area = length * width
+
+print("The area of the rectangle is:", area)
--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,9 @@
+#variables
 principal = 1000
 rate = 0.05
 time = 3
 
+#calculate simple interest
 interest = principal * rate * time
+
 print("The simple interest is:", interest)
