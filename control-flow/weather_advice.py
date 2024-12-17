70fdf39e551be9b2e5ceb63a0aa1292bb439d5c7
Deelan Kilel <kileldylan@gmail.com>
2024-12-17 15:24:01 +0300

    Programm to predict type of clothes to wear in certain weather conditions

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,15 +1,18 @@
 def weather_advices():
     print("Welcome to this weather advice program")
     
+    # Prompt user for the weather
     weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
     
+    # Check weather conditions and provide advice
     if weather == "sunny":
-        print("Wear a T-shirt and sunglasses ")
+        print("Wear a T-shirt and sunglasses.")
     elif weather == "rainy":
-        print("Don't forget your umbrella and a raincoat")
+        print("Don't forget your umbrella and a raincoat.")
     elif weather == "cold":
-        print("Make sure to wear a warm coat and a scarf")
+        print("Make sure to wear a warm coat and a scarf.")
     else:
-        print("Sorry, I don't have recommendations for this weather")
-        
+        print("Sorry, I don't have recommendations for this weather.")
+
+# Call the function
 weather_advices()
