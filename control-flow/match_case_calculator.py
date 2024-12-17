2a1a513449ed8b2cf09b298180a3adbf01126701
Deelan Kilel <kileldylan@gmail.com>
2024-12-17 15:52:20 +0300

    programm to handle different operations

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,23 @@
+#program to handle multiple calculations with diff operations.
+
+def match_case_calculator():
+    print("Welcome to our calculator")
+    
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    operation = input("Choose operation (+, -, *, /)")
+    
+    match operation:
+        case _ if operation == "+":
+            calculation = num1 + num2
+            print("The result is :", calculation)
+        case _ if operation == "-":
+            calculation = num1 - num2
+            print("The result is :", calculation)
+        case _ if operation == "*":
+            calculation = num1 * num2
+            print("The result is :", calculation)
+        case _ if operation == "/":
+            calculation = num1 / num2
+            print("The result is :", calculation)
+match_case_calculator()
