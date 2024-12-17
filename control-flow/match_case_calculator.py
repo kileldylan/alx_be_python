5662641de9e55df99239e76aa3b438fc2f7e8063
Deelan Kilel <kileldylan@gmail.com>
2024-12-17 16:01:12 +0300

    programm to handle different operations

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -5,7 +5,7 @@ def match_case_calculator():
     
     num1 = int(input("Enter the first number: "))
     num2 = int(input("Enter the second number: "))
-    operation = input("Choose operation (+, -, *, /)")
+    operation = input("Choose the operation (+, -, *, /): ")
     
     match operation:
         case _ if operation == "+":
