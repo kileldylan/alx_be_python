09da5d6da04bf5f23e1f9616f603292f77e71148
Deelan Kilel <kileldylan@gmail.com>
2025-01-15 14:34:55 +0300

    shopping list manager program

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -16,7 +16,7 @@ def main():
         choice = input("Enter your choice: ").strip()
         
         if choice == "1":
-            item = input(" Enter the item you're adding to the list: ").strip()
+            item = input(" Enter the item to add: ").strip()
             if item:
                 shopping_list.append(item)
                 print(f"'{item}' has been added to the shopping list succesfully!")
