ff6bc300d1fb9c09fb91a32a59960f0936d3e5e7
Deelan Kilel <kileldylan@gmail.com>
2025-01-15 13:45:27 +0300

    arithmetic operations program to link with main.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+# arithmetic operations programm
+
+def perfom_operation(num1, num2, operation):
+    
+    if operation == "add":
+        return num1+num2
+    elif operation == "subtract":
+        return num1-num2
+    elif operation == "multiply":
+        return num1*num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Cant divide by 0"
+        return num1/num2
+    else:
+        return "Invalid operation used!"
+    
