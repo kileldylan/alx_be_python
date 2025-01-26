b42702134246bc359c3d4f36c1f427fc231d3673
Deelan Kilel <kileldylan@gmail.com>
2025-01-26 10:08:31 +0300

    script to practice the use of magic methods

--- oop/book_class.py
+++ oop/book_class.py
@@ -9,7 +9,7 @@ class Book:
         self.year = year
     
     def __del__(self):
-        print("Book has been deleted")
+        print(f"Deleting {self.year}")
     
     def __str__(self):
         return f"{self.title} by {self.author} published in {self.year}"
