ac357614002a0b82f23c4eb092fc71d25832b96d
Deelan Kilel <kileldylan@gmail.com>
2025-01-26 11:09:42 +0300

    made changes to the naming of filesize to file_size to fit the expected output

--- oop/library_system.py
+++ oop/library_system.py
@@ -10,7 +10,7 @@ class Book:
 class EBook(Book):
     def __init__(self,title,author,file_size):
         super().__init__(title,author)
-        self.filesize = file_size
+        self.file_size = file_size
     def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
 
